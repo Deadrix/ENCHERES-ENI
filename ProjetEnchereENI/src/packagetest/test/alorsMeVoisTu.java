c07f02afe0a78949e85d7d2eb940f8c4f1b671f9
Xav ™ <Xav @DESKTOP-NET5CKA>
2022-03-21 16:50:27 +0100

    test

--- ProjetEnchereENI/src/packagetest/test/alorsMeVoisTu.java
+++ ProjetEnchereENI/src/packagetest/test/alorsMeVoisTu.java
@@ -3,7 +3,7 @@ package packagetest.test;
 public class alorsMeVoisTu {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("YES I can see you!");
 
 	}
 
