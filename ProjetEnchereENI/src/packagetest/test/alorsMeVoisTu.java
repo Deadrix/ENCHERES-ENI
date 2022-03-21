a59433dcaac2d77760c08d8a6dbb72de50ae8e67
Deadrix <Alexandre66480@hotmail.fr>
2022-03-21 18:20:25 +0100

    alors ?

--- ProjetEnchereENI/src/packagetest/test/alorsMeVoisTu.java
+++ ProjetEnchereENI/src/packagetest/test/alorsMeVoisTu.java
@@ -4,22 +4,17 @@ public class alorsMeVoisTu {
 
 	public static void main(String[] args) {
 		System.out.println("YES I can see you!");
-		
-		for(int i=0; i<10;i++) {
+
+		for (int i = 0; i < 10; i++) {
 			System.out.println("OKAY");
-			
-		System.out.println("It works!");
-		System.out.println("Oh Yeah");
-		System.out.println("Oh no");
-		System.out.println("he heeeeee");
-		
-		
-<<<<<<< HEAD
-		
-		System.out.println("peu importe");
-=======
-		System.out.println("he hoooooo");
->>>>>>> branch 'main' of https://github.com/Deadrix/ENCHERES-ENI.git
+
+			System.out.println("It works!");
+			System.out.println("Oh Yeah");
+			System.out.println("Oh no");
+			System.out.println("he heeeeee");
+			System.out.println("peu importe");
+			System.out.println("he hoooooo");
+
 		}
 	}
 
