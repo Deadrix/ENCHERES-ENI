d741ae8533acb1293bee56b2b2020f02de467d26
Deadrix <Alexandre66480@hotmail.fr>
2022-03-21 16:24:25 +0100

    restart

--- ProjetEnchereENI/src/main/java/com/dreamteam/test/testKam.java
+++
@@ -1,9 +0,0 @@
-package com.dreamteam.test;
-
-public class testKam {
-
-	public static void main(String[] args) {
-		
-		System.out.println("sup");
-	}
-}
--- ProjetEnchereENI/src/test/packagetest/ouestelledeadrix.java
+++ ProjetEnchereENI/src/test/packagetest/ouestelledeadrix.java
@@ -3,13 +3,9 @@ package test.packagetest;
 public class ouestelledeadrix {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		System.out.println("sup");
 
-=======
-		// TODO Auto-generated method stub
 		System.out.println("hello");
->>>>>>> branch 'main' of https://github.com/Deadrix/ENCHERES-ENI.git
+
 	}
 
 }
