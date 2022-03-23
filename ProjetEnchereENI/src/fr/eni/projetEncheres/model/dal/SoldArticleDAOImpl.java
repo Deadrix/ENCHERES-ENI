d5924c5f7229da65d7f5ace782479bcb8c154a0f
Deadrix <Alexandre66480@hotmail.fr>
2022-03-23 11:47:27 +0100

    Update SQL Command SoldArticleDAOImpl

--- ProjetEnchereENI/src/fr/eni/projetEncheres/model/dal/SoldArticleDAOImpl.java
+++ ProjetEnchereENI/src/fr/eni/projetEncheres/model/dal/SoldArticleDAOImpl.java
@@ -5,6 +5,13 @@ import java.util.List;
 import fr.eni.projetEncheres.model.bo.SoldArticle;
 
 public class SoldArticleDAOImpl implements DAO<SoldArticle> {
+	
+	private final String SQLINSERT="INSERT INTO ARTICLES_VENDUS (nom_article,description,date_debut_encheres,date_fin_encheres,prix_initial,prix_vente,no_utilisateur,no_categorie) VALUES (?,?,?,?,?,?,?,?)";
+	private final String SQLUPDATE="UPDATE ARTICLES_VENDUS set nom_article=?,description=?,date_debut_encheres=?,date_fin_encheres=?,prix_initial,prix_vente=?,no_utilisateur=?,no_categorie=? where id=?";
+	private final String SQLDELETEBYID="DELETE FROM ARTICLES_VENDUS WHERE id=?";
+	private final String SQLSELECTALL="SELECT nom_article,description,date_debut_encheres,date_fin_encheres,prix_initial,prix_vente,no_utilisateur,no_categorie FROM ARTICLES_VENDUS";
+	private final String SQLSELECTBYID="SELECT nom_article,description,date_debut_encheres,date_fin_encheres,prix_initial,prix_vente,no_utilisateur,no_categorie FROM ARTICLES_VENDUS where id=?";
+	private static SoldArticleDAOImpl instance = null;
 
 	@Override
 	public void insert(SoldArticle object) throws DALException {
