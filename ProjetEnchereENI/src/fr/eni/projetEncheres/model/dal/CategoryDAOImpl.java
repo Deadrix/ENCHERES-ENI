5a07df98d3049f4c2f802cbbb99f4f6504e12e81
Deadrix <Alexandre66480@hotmail.fr>
2022-03-23 11:35:03 +0100

    Update sql command CategoryDAOImpl

--- ProjetEnchereENI/src/fr/eni/projetEncheres/model/dal/CategoryDAOImpl.java
+++ ProjetEnchereENI/src/fr/eni/projetEncheres/model/dal/CategoryDAOImpl.java
@@ -5,6 +5,13 @@ import java.util.List;
 import fr.eni.projetEncheres.model.bo.Category;
 
 public class CategoryDAOImpl implements DAO<Category> {
+	
+	private final String SQLINSERT="INSERT INTO Categories (libelle) VALUES (?)";
+	private final String SQLUPDATE="UPDATE Categories set libelle=? where id=?";
+	private final String SQLDELETEBYID="DELETE FROM Categories WHERE id=? ";
+	private final String SQLSELECTALL="SELECT libelle FROM Categories";
+	private final String SQLSELECTBYID="SELECT libelle FROM Categories where id=?";
+	private static CategoryDAOImpl instance = null;
 
 	@Override
 	public void insert(Category object) throws DALException {
@@ -41,5 +48,5 @@ public class CategoryDAOImpl implements DAO<Category> {
 		// TODO Auto-generated method stub
 		
 	}
-
+	
 }
