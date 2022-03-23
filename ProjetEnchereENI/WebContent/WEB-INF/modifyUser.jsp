929e784f27be622b74753cd396a27c220add7a60
Deadrix <Alexandre66480@hotmail.fr>
2022-03-23 12:58:23 +0100

    ADD uncomplete modify user function

---
+++ ProjetEnchereENI/WebContent/WEB-INF/modifyUser.jsp
@@ -0,0 +1,38 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ page import="fr.eni.projetEncheres.model.bo.User" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Modifier Utilisateur</title>
+</head>
+<body>
+
+
+<form action="ServletModifyUser" method="POST">
+<input type="hidden" name="userId" value="${user.id}"/>
+<label for="pseudo">Pseudo: </label><input type="text" id="pseudo" name="txtAlias" value="${user.alias}"/>
+<br>
+<label for="lastName">Nom: </label><input type="text" id="lastName"" name="txtLastName" value="${user.lastName}"/>
+<br>
+<label for="firstName">Prenom: </label><input type="text" id="firstName" name="txtFirstName" value="${user.firstName}"/>
+<br>
+<label for="email">Email: </label><input type="text" id="email" name="txtEmail" value="${user.email}"/>
+<br>
+<label for="phone">Numéro de téléphone: </label><input type="tel" id="phone" name="txtPhone" value="${user.telephone}" pattern="[0-9]{10}" required>
+<br>
+<label for="street">Adresse: </label><input type="text" id="street" name="txtStreet" value="${user.street}"/>
+<br>
+<label for="postalCode">Code Postal: </label><input type="text" id="postalCode" name="txtpostalCode" value="${user.postalCode}"/>
+<br>
+<label for="city">Ville: </label><input type="text" id="city"" name="txtcity" value="${user.city}"/>
+<br>
+
+
+<input type="submit" name="valider" value="Enregistrer Modification"/>
+</form> 
+
+
+</body>
+</html>
---
+++ ProjetEnchereENI/src/fr/eni/projetEncheres/controller/ServletModifyUser.java
@@ -0,0 +1,72 @@
+package fr.eni.projetEncheres.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import eni.annuaire.bll.UtilisateurManager;
+import eni.annuaire.bo.Utilisateur;
+import fr.eni.projetEncheres.model.bo.User;
+
+/**
+ * Servlet implementation class ServletModifyUser
+ */
+@WebServlet("/ServletModifyUser")
+public class ServletModifyUser extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		int userId;
+		userId=Integer.parseInt(request.getParameter("id"));
+		User u=null;
+		u=UserManager.getInstance().findById(userId);
+		request.setAttribute("user", u);
+		getServletContext().getRequestDispatcher("/WEB-INF/modifyUser.jsp").forward(request, response);
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
+		int userId;		
+		String alias,lastName,firstName,email,telephone,street,postalCode,city,password;		
+		User u;
+		
+		userId=Integer.parseInt(request.getParameter("userId"));
+		alias=request.getParameter("txtAlias");
+		lastName=request.getParameter("txtLastName");
+		firstName=request.getParameter("txtFirstName");
+		email=request.getParameter("txtEmail");
+		telephone=request.getParameter("txtTelephone");
+		street=request.getParameter("txtStreet");
+		postalCode=request.getParameter("txtPostalCode");
+		city=request.getParameter("txtCity");
+		password=request.getParameter("txtPassword");
+		
+		
+		u = UserManager.getInstance().findById(userId);
+		
+		u.setLastName(lastName);
+		u.setFirstName(firstName);
+		u.setEmail(email);
+		u.setTelephone(telephone);
+		u.setStreet(street);
+		u.setPostalCode(postalCode);
+		u.setCity(city);
+		u.setPassword(password);
+		
+		UserManager.getInstance().modifier(u);
+		response.sendRedirect("index.html");
+	}
+
+}
