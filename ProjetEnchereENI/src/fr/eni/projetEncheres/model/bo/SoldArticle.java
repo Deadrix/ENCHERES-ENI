7f391847537ccc841c4d8a773c20ebacbe81325d
Xav ™ <Xav @DESKTOP-NET5CKA>
2022-03-24 16:19:03 +0100

    Finally SoldArticle

--- ProjetEnchereENI/src/fr/eni/projetEncheres/model/bo/SoldArticle.java
+++ ProjetEnchereENI/src/fr/eni/projetEncheres/model/bo/SoldArticle.java
@@ -1,116 +1,162 @@
 package fr.eni.projetEncheres.model.bo;
 
 import java.io.Serializable;
+import java.time.LocalDate;
 import java.util.Date;
 
 public class SoldArticle implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
-	private Integer articleId;
+	private int idArticle;
 	private String articleName;
 	private String description;
 	private Date auctionStart;
 	private Date auctionEnd;
-	private Integer initialPrice;
-	private Integer soldPrice;
-	private User buyer;
-	private User seller;
-
+	private int initialPrice;
+	private int soldPrice;
 	private Category category;
-
+	private User seller;
+	private User buyer;
+	
+	
 	public SoldArticle() {
 		super();
 	}
 
-	public SoldArticle(String articleName, String description, Date auctionStart, Date auctionEnd, Integer initialPrice,
-			Category category) {
+
+	public SoldArticle(int idArticle, String articleName, String description, Date auctionStart, Date auctionEnd,
+			int initialPrice, int soldPrice, Category category, User seller, User buyer) {
 		super();
+		this.idArticle = idArticle;
 		this.articleName = articleName;
 		this.description = description;
 		this.auctionStart = auctionStart;
 		this.auctionEnd = auctionEnd;
 		this.initialPrice = initialPrice;
+		this.soldPrice = soldPrice;
 		this.category = category;
+		this.seller = seller;
+		this.buyer = buyer;
 	}
+
+// Constructeur sans buyer pour l'initialiser plus tard (à l'achat de l'article)
+	public SoldArticle(int idArticle, String articleName, String description, Date auctionStart, Date auctionEnd,
+			int initialPrice, int soldPrice, Category category, User seller) {
+		super();
+		this.idArticle = idArticle;
+		this.articleName = articleName;
+		this.description = description;
+		this.auctionStart = auctionStart;
+		this.auctionEnd = auctionEnd;
+		this.initialPrice = initialPrice;
+		this.soldPrice = soldPrice;
+		this.category = category;
+		this.seller = seller;
+	}
+
+
+	public int getIdArticle() {
+		return idArticle;
+	}
+
+
+	public void setIdArticle(int idArticle) {
+		this.idArticle = idArticle;
+	}
+
 
 	public String getArticleName() {
 		return articleName;
 	}
 
+
 	public void setArticleName(String articleName) {
 		this.articleName = articleName;
 	}
+
 
 	public String getDescription() {
 		return description;
 	}
 
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
+
 
 	public Date getAuctionStart() {
 		return auctionStart;
 	}
 
+
 	public void setAuctionStart(Date auctionStart) {
 		this.auctionStart = auctionStart;
 	}
+
 
 	public Date getAuctionEnd() {
 		return auctionEnd;
 	}
 
+
 	public void setAuctionEnd(Date auctionEnd) {
 		this.auctionEnd = auctionEnd;
 	}
 
-	public Integer getInitialPrice() {
+
+	public int getInitialPrice() {
 		return initialPrice;
 	}
 
-	public void setInitialPrice(Integer initialPrice) {
+
+	public void setInitialPrice(int initialPrice) {
 		this.initialPrice = initialPrice;
 	}
+
+
+	public int getSoldPrice() {
+		return soldPrice;
+	}
+
+
+	public void setSoldPrice(int soldPrice) {
+		this.soldPrice = soldPrice;
+	}
+
 
 	public Category getCategory() {
 		return category;
 	}
 
+
 	public void setCategory(Category category) {
 		this.category = category;
 	}
 
-	public Integer getSoldPrice() {
-		return soldPrice;
-	}
-
-	public void setSoldPrice(Integer soldPrice) {
-		this.soldPrice = soldPrice;
-	}
-
-	public Integer getArticleId() {
-		return articleId;
-	}
-
-	public void setArticleId(Integer articleId) {
-		this.articleId = articleId;
-	}
-
-	public User getBuyer() {
-		return buyer;
-	}
-
-	public void setBuyer(User buyer) {
-		this.buyer = buyer;
-	}
 
 	public User getSeller() {
 		return seller;
 	}
 
+
 	public void setSeller(User seller) {
 		this.seller = seller;
 	}
+
+
+	public User getBuyer() {
+		return buyer;
+	}
+
+
+	public void setBuyer(User buyer) {
+		this.buyer = buyer;
+	}
+	
+	
+	
+	
+	
 }
--- ProjetEnchereENI/src/fr/eni/projetEncheres/model/dal/SoldArticleDAOImpl.java
+++ ProjetEnchereENI/src/fr/eni/projetEncheres/model/dal/SoldArticleDAOImpl.java
@@ -1,51 +1,254 @@
 package fr.eni.projetEncheres.model.dal;
 
+import java.beans.Statement;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
+import com.sun.xml.internal.ws.org.objectweb.asm.Type;
+
+import fr.eni.projetEncheres.model.bll.UserManager;
 import fr.eni.projetEncheres.model.bo.SoldArticle;
 
 public class SoldArticleDAOImpl implements DAO<SoldArticle> {
 	
-	private final String SQLINSERT="INSERT INTO ARTICLES_VENDUS (nom_article,description,date_debut_encheres,date_fin_encheres,prix_initial,prix_vente,no_utilisateur,no_categorie) VALUES (?,?,?,?,?,?,?,?)";
-	private final String SQLUPDATE="UPDATE ARTICLES_VENDUS set nom_article=?,description=?,date_debut_encheres=?,date_fin_encheres=?,prix_initial,prix_vente=?,no_utilisateur=?,no_categorie=? where id=?";
-	private final String SQLDELETEBYID="DELETE FROM ARTICLES_VENDUS WHERE id=?";
-	private final String SQLSELECTALL="SELECT nom_article,description,date_debut_encheres,date_fin_encheres,prix_initial,prix_vente,no_utilisateur,no_categorie FROM ARTICLES_VENDUS";
-	private final String SQLSELECTBYID="SELECT nom_article,description,date_debut_encheres,date_fin_encheres,prix_initial,prix_vente,no_utilisateur,no_categorie FROM ARTICLES_VENDUS where id=?";
 	private static SoldArticleDAOImpl instance = null;
 
+	private final String SQLINSERT="INSERT INTO ARTICLES_VENDUS (nom_article,description,date_debut_encheres,date_fin_encheres,prix_initial,prix_vente,no_vendeur,no_acheteur,no_categories) VALUES(?,?,?,?,?,?,?,?,?)";
+	private final String SQLUPDATE="UPDATE ARTICLES_VENDUS set nom_article=?, description=?, date_debut_encheres=?, date_fin_encheres=?, prix_initial=?, prix_vente=?, no_vendeur=?, no_acheteur=?, no_categorie=? WHERE id=?";
+	private final String SQLDELETEBYID="DELETE FROM ARTICLES_VENDUS WHERE id=?";
+	private final String SQLSELECTBYID="SELECT no_article, nom_article,description,date_debut_encheres,date_fin_encheres,prix_initial,prix_vente,no_utilisateur,no_categorie FROM ARTICLES_VENDUS where id=?";
+	private final String SQLSELECTALL="SELECT no_article, nom_article,description,date_debut_encheres,date_fin_encheres,prix_initial,prix_vente,no_utilisateur,no_categorie FROM ARTICLES_VENDUS";
+	
+	
 	@Override
 	public void insert(SoldArticle object) throws DALException {
-		// TODO Auto-generated method stub
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		try {
+			con=ConnectionProvider.getConnection();
+			pstmt=con.prepareStatement(SQLINSERT, PreparedStatement.RETURN_GENERATED_KEYS);
+			pstmt.setString(1, object.getArticleName());
+			pstmt.setString(2, object.getDescription());
+			pstmt.setDate(3, (Date) object.getAuctionStart());
+			pstmt.setDate(4, (Date) object.getAuctionEnd());	
+			if(object.getAuctionStart()==null) {
+				pstmt.setNull(5, Type.INT);
+			}
+			else {
+				pstmt.setInt(5, object.getInitialPrice());
+			}
+			if(object.getAuctionStart()==null) {
+				pstmt.setNull(6, Type.INT);
+			}
+			else {
+				pstmt.setInt(6, object.getSoldPrice());
+			}
+			
+			pstmt.setInt(7, object.getSeller().getUserId());
+			pstmt.setInt(8, object.getBuyer().getUserId());
+			pstmt.setInt(9, object.getCategory().getCategoryId());
+			
+			pstmt.executeUpdate();
+			con.close();
+			
+		} 
+		catch (SQLException e) {
+			e.printStackTrace();
+			throw new DALException("DATA ACCESS LAYER EXCEPTION : SoldArticle insertion into database failed - ", e);
+		}
 		
+		finally {
+			try {
+				if (pstmt != null) {
+					pstmt.close();
+				}
+				if (con != null) {
+					con.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+				throw new DALException("Close failed - ", e);
+			}
+		}
 	}
 
 	@Override
 	public void update(SoldArticle object) throws DALException {
-		// TODO Auto-generated method stub
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		try {
+			con=ConnectionProvider.getConnection();
+			pstmt=con.prepareStatement(SQLUPDATE);
+			pstmt.setString(1, object.getArticleName());
+			pstmt.setString(2, object.getDescription());
+			pstmt.setDate(3, (Date) object.getAuctionStart());
+			pstmt.setDate(4, (Date) object.getAuctionEnd());	
+			if(object.getAuctionStart()==null) {
+				pstmt.setNull(5, Type.INT);
+			}
+			else {
+				pstmt.setInt(5, object.getInitialPrice());
+			}
+			if(object.getAuctionStart()==null) {
+				pstmt.setNull(6, Type.INT);
+			}
+			else {
+				pstmt.setInt(6, object.getSoldPrice());
+			}
+			
+			pstmt.setObject(7, object.getBuyer());
+			pstmt.setObject(8, object.getCategory());
+			
+			pstmt.executeUpdate();
+			con.close();
+			
+		} 
+		catch (SQLException e) {
+			e.printStackTrace();
+			throw new DALException("DATA ACCESS LAYER EXCEPTION : SoldArticle insertion into database failed - ", e);
+		}
+		
+		finally {
+			try {
+				if (pstmt != null) {
+					pstmt.close();
+				}
+				if (con != null) {
+					con.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+				throw new DALException("Close failed - ", e);
+			}
+		}
 		
 	}
 
 	@Override
 	public SoldArticle selectById(int idObject) throws DALException {
-		// TODO Auto-generated method stub
-		return null;
-	}
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		SoldArticle art = null;
+		ResultSet rs;
+		
+		try {
+			con = ConnectionProvider.getConnection();
+			pstmt = con.prepareStatement(SQLDELETEBYID);
+			pstmt.setInt(1, idObject);
+			rs = pstmt.executeQuery(SQLSELECTBYID);
+			if(rs.next()) 
+			{
+				art = new SoldArticle(rs.getInt("no_article"),
+									  rs.getString("nom_article"),
+									  rs.getString("description"),
+									  rs.getDate("date_debut_encheres"),
+									  rs.getDate("date_fin_encheres"),
+									  rs.getInt("prix_initial"),
+									  rs.getInt("prix_vente"),									  
+									  UserManager.getInstance().selectById(rs.getInt("no_vendeur")),
+									  UserManager.getInstance().selectById(rs.getInt("no_acheteur")),
+									  CategoryManager.getInstance().selectById(rs.getInt("no_categorie"));
+				return art;  		
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new DALException("DATA ACCESS LAYER EXCEPTION : SoldArticle selectionById from database failed - ", e);
+		} finally {
+			try {
+				if (pstmt != null) {
+					pstmt.close();
+				}
+				if (con != null) {
+					con.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+				throw new DALException("Close failed - ", e);
+			}
+		}
+		return art;
+		}
+		
+	
 
 	@Override
 	public List<SoldArticle> selectAll() throws DALException {
-		// TODO Auto-generated method stub
-		return null;
-	}
+		Connection con = null;
+		java.sql.Statement stmt = null;
+		List<SoldArticle> lst;
+		lst = new ArrayList<>();
+		ResultSet rs;
+		SoldArticle soldArticle;
+				
+			try {
+				con = ConnectionProvider.getConnection();
+				stmt = con.createStatement();
+				rs = stmt.executeQuery(SQLSELECTALL);
+				while(rs.next()) 
+				{
+					soldArticle = new SoldArticle(rs.getInt("no_article"),
+												  rs.getString("nom_article"),
+												  rs.getString("description"),
+												  rs.getDate("date_debut_encheres"),
+												  rs.getDate("date_fin_encheres"),
+												  rs.getInt("prix_initial"),
+												  rs.getInt("prix_vente"),									  
+												  UserManager.getInstance().selectById(rs.getInt("no_vendeur")),
+												  UserManager.getInstance().selectById(rs.getInt("no_acheteur")),
+												  CategoryManager.getInstance().selectById(rs.getInt("no_categorie"));
+					lst.add(soldArticle);
+				}
+						
+			} catch (SQLException e) {
+				e.printStackTrace();
+				throw new DALException("DATA ACCESS LAYER EXCEPTION : All SoldArticle selection from database failed - ", e);
+			} finally {
+				try {
+					if (stmt != null) {
+						stmt.close();
+					}
+					if (con != null) {
+						con.close();
+					}
+				} catch (SQLException e) {
+					e.printStackTrace();
+					throw new DALException("Close failed - ", e);
+				}
+			}
+			return lst;
+			}
+		
+	
 
 	@Override
 	public List<SoldArticle> selectByMotCle(String motCle) throws DALException {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public void delete(int idObject) throws DALException {
-		// TODO Auto-generated method stub
+		Connection con;
+		PreparedStatement pstmt;
+		try {
+			con=ConnectionProvider.getConnection();
+			pstmt=con.prepareStatement(SQLDELETEBYID);
+			pstmt.setInt(1, idObject);
+						
+			pstmt.executeUpdate();
+			con.close();
+			
+		} 
+		catch (SQLException e) {
+			e.printStackTrace();
+			throw new DALException("DATA ACCESS LAYER EXCEPTION : SoldArticle deletion into database failed - ", e);
+		}
+		
+	
 		
 	}
 
