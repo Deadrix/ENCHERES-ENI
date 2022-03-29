44f5abc2da98af9d0126dab13e80c0c6f98b9a83
Deadrix <Alexandre66480@hotmail.fr>
2022-03-29 16:17:36 +0200

    Update db connection

--- ProjetEnchereENI/src/fr/eni/projetEncheres/model/dal/PickUpDAOImpl.java
+++ ProjetEnchereENI/src/fr/eni/projetEncheres/model/dal/PickUpDAOImpl.java
@@ -8,201 +8,132 @@ import java.util.ArrayList;
 import java.util.List;
 
 import fr.eni.projetEncheres.model.bo.PickUp;
+import fr.eni.projetEncheres.model.bo.SoldArticle;
 
 public class PickUpDAOImpl implements DAO<PickUp> {
-	
+
 //	no_article         INTEGER NOT NULL,
 //    rue              VARCHAR(30) NOT NULL,
 //    code_postal      VARCHAR(15) NOT NULL,
 //    ville            VARCHAR(30) NOT NULL
-	
 
-	private static final String INSERT ="INSERT INTO RETRAITS(rue, code_postal, ville) VALUES (?, ?, ?);";
-	private static final String UPDATE ="UPDATE RETRAITS SET "
-			+ "rue = ?, "
-			+ "code_postal = ?, "
-			+ "ville = ? "
+	private static final String INSERT = "INSERT INTO RETRAITS(rue, code_postal, ville) VALUES (?, ?, ?);";
+	private static final String UPDATE = "UPDATE RETRAITS SET " + "rue = ?, " + "code_postal = ?, " + "ville = ? "
 			+ "WHERE no_article = ?;";
-	private static final String DELETE ="DELETE FROM PICKUPS WHERE no_article = ?;";
-	private static final String SELECT_ALL ="SELECT * FROM PICKUPS";
-	private static final String SELECT_BY_ID ="SELECT * FROM PICKUPS WHERE no_article = ?;";
-	private static final String SELECT_BY_KEYWORD = "SELECT rue, code_postal, ville"
+	private static final String DELETE = "DELETE FROM PICKUPS WHERE no_article = ?;";
+	private static final String SELECTBYID = "SELECT * FROM PICKUPS WHERE no_article = ?;";
+	private static final String SELECTALL = "SELECT * FROM PICKUPS";
+	private static final String SELECTBYKEYWORD = "SELECT rue, code_postal, ville"
 			+ " FROM RETRAITS WHERE rue like ? or ville like ? or code_postal like ?";
-	
 
-	@Override
 	public void insert(PickUp object) throws DALException {
-		Connection con = null;
-		PreparedStatement pstmt = null;
-		try {
-			con = ConnectionProvider.getConnection();
-			pstmt = con.prepareStatement(INSERT);
-			pstmt.setString(1, object.getRueRetrait());
-			pstmt.setString(2, object.getCodePostalRetrait());
-			pstmt.setString(3, object.getVilleRetrait());
-			pstmt.executeUpdate();
-			
+
+		try (Connection connect = ConnectionProvider.getConnection();
+				PreparedStatement ps = connect.prepareStatement(INSERT)) {
+			ps.setString(1, object.getRueRetrait());
+			ps.setString(2, object.getCodePostalRetrait());
+			ps.setString(3, object.getVilleRetrait());
+			ps.executeUpdate();
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 			throw new DALException("DATA ACCESS LAYER EXCEPTION : Insert Pick-Up into database failed - ", e);
-		} finally {
-			try {
-				if (pstmt != null) {
-					pstmt.close();
-				}
-				if (con != null) {
-					con.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-				throw new DALException("Close failed - ", e);
-			}
 		}
 	}
 
-	@Override
 	public void update(PickUp object) throws DALException {
-		Connection con = null;
-		PreparedStatement pstmt = null;
-		try {
-			con = ConnectionProvider.getConnection();
-			pstmt = con.prepareStatement(UPDATE);
-			pstmt.setString(1, object.getRueRetrait());
-			pstmt.setString(2, object.getCodePostalRetrait());
-			pstmt.setString(3, object.getVilleRetrait());
-			pstmt.executeUpdate();
+		try (Connection connect = ConnectionProvider.getConnection();
+				PreparedStatement ps = connect.prepareStatement(UPDATE)) {
+
+			ps.setString(1, object.getRueRetrait());
+			ps.setString(2, object.getCodePostalRetrait());
+			ps.setString(3, object.getVilleRetrait());
+			ps.executeUpdate();
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 			throw new DALException("DATA ACCESS LAYER EXCEPTION : Update Pick-Up into database failed", e);
-		} finally {
-			try {
-				if (pstmt != null) {
-					pstmt.close();
-				}
-				if (con != null) {
-					con.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-				throw new DALException("Close failed - ", e);
-			}
-		}		
+		}
 	}
 
-	@Override
 	public PickUp selectById(int idSoldArticle) throws DALException {
+
 		PickUp pu = null;
-		Connection con = null;
-		PreparedStatement pstmt = null;
-		try {
-			con = ConnectionProvider.getConnection();
-			pstmt = con.prepareStatement(SELECT_BY_ID);
-			pstmt.setInt(1, idSoldArticle);
-			ResultSet rs = pstmt.executeQuery();
-			if(rs.next()) {
-				pu = new PickUp(
-						rs.getString(2),
-						rs.getString(3),
-						rs.getString(4)
-						);
+
+		try (Connection connect = ConnectionProvider.getConnection();
+				PreparedStatement ps = connect.prepareStatement(SELECTBYID)) {
+			ps.setInt(1, idSoldArticle);
+			ResultSet rs = ps.executeQuery();
+			if (rs.next()) {
+				pu = new PickUp(rs.getString(2), rs.getString(3), rs.getString(4));
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 			throw new DALException("DATA ACCESS LAYER EXCEPTION : Select Pick-Up by Id from database failed - ", e);
-		} finally {
-			try {
-				if (pstmt != null) {
-					pstmt.close();
-				}
-				if (con != null) {
-					con.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-				throw new DALException("Close failed - ", e);
-			}
 		}
 		return pu;
 	}
 
-	@Override
 	public List<PickUp> selectAll() throws DALException {
-		List<PickUp> list = new ArrayList<>();
-		Connection con = null;
-		PreparedStatement pstmt = null;
-		try {
-			con = ConnectionProvider.getConnection();
-			pstmt = con.prepareStatement(SELECT_ALL);
-			ResultSet rs = pstmt.executeQuery();
-			while(rs.next()) {
-				PickUp pu = new PickUp(
-						rs.getString(2),
-						rs.getString(3),
-						rs.getString(4)
-						);
-				list.add(pu);
+
+		List<PickUp> lst = new ArrayList<>();
+		lst = new ArrayList<>();
+		PickUp pu = null;
+
+		try (Connection connect = ConnectionProvider.getConnection();
+				PreparedStatement ps = connect.prepareStatement(SELECTALL)) {
+
+			ResultSet rs = ps.executeQuery();
+
+			while (rs.next()) {
+				pu = new PickUp(rs.getString(2), rs.getString(3), rs.getString(4));
+				lst.add(pu);
 			}
+
 		} catch (SQLException e) {
 			e.printStackTrace();
-			throw new DALException("DATA ACCESS LAYER EXCEPTION : Select All Pick-Ups selection from database failed - ", e);
-		} finally {
-			try {
-				if (pstmt != null) {
-					pstmt.close();
-				}
-				if (con != null) {
-					con.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-				throw new DALException("Close failed - ", e);
-			}
+			throw new DALException(
+					"DATA ACCESS LAYER EXCEPTION : Select All Pick-Ups selection from database failed - ", e);
 		}
-		return list;
+		return lst;
 	}
 
 	public List<PickUp> selectByMotCle(String motCle) throws DALException {
-		Connection connect = null;
-		PreparedStatement pStmt = null;
-		ResultSet rs = null;
-		List<PickUp> liste = new ArrayList<PickUp>();
-		try {
-			connect = ConnectionProvider.getConnection();
-			pStmt = connect.prepareStatement(SELECT_BY_KEYWORD);
-			pStmt.setString(1, motCle);
-			rs = pStmt.executeQuery();
-			while (rs.next()) {
-				PickUp pu = new PickUp(
-						rs.getString(2),
-						rs.getString(3),
-						rs.getString(4)
-						);
+		
+		List<PickUp> lst = new ArrayList<>();
+		lst = new ArrayList<>();
+		PickUp pu = null;
+
+		try (Connection connect = ConnectionProvider.getConnection();
+				PreparedStatement ps = connect.prepareStatement(SELECTBYKEYWORD)) {
 			
-				liste.add(pu);
+			ps.setString(1, motCle);
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+				pu = new PickUp(rs.getString(2), rs.getString(3), rs.getString(4));
+
+				lst.add(pu);
 			}
+			
 		} catch (SQLException e) {
 			throw new DALException("selectByMotCle failed - ", e);
-		} 
-		return liste;
-		
+		}
+		return lst;
 	}
 
-	@Override
 	public void delete(int idArticleVendu) throws DALException {
-		Connection con = null;
-		PreparedStatement pstmt = null;
+		Connection connect = null;
+		PreparedStatement ps = null;
 		try {
-			con = ConnectionProvider.getConnection();
-			pstmt = con.prepareStatement(DELETE);
-			pstmt.setInt(1, idArticleVendu);
-			pstmt.executeUpdate();
+			connect = ConnectionProvider.getConnection();
+			ps = connect.prepareStatement(DELETE);
+			ps.setInt(1, idArticleVendu);
+			ps.executeUpdate();
+			
 		} catch (SQLException e) {
 			e.printStackTrace();
 			throw new DALException("DATA ACCESS LAYER EXCEPTION : Delete Pick-up from database failed - ", e);
 		}
-		
 	}
-	
-	
 
 }
