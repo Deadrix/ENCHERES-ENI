a290e02b49c50f451f17298fcfbce7039f59fdc8
Deadrix <Alexandre66480@hotmail.fr>
2022-03-31 10:38:01 +0200

    Mise en page script

--- Scripts SQL Server/Suppression Enchere ENI.sql
+++ Scripts SQL Server/Suppression Enchere ENI.sql
@@ -1,15 +1,70 @@
-IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[RETRAITS]') AND type in (N'U'))
-DROP TABLE [dbo].[RETRAITS]
+IF EXISTS
+    (
+        SELECT
+            *
+        FROM
+            sys.objects
+        WHERE
+            object_id = OBJECT_ID(N'[dbo].[RETRAITS]')
+        AND type IN (
+                        N'U'
+                    )
+    )
+    DROP TABLE [dbo].[RETRAITS]
 GO
-IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[ENCHERES]') AND type in (N'U'))
-DROP TABLE [dbo].[ENCHERES]
+IF EXISTS
+    (
+        SELECT
+            *
+        FROM
+            sys.objects
+        WHERE
+            object_id = OBJECT_ID(N'[dbo].[ENCHERES]')
+        AND type IN (
+                        N'U'
+                    )
+    )
+    DROP TABLE [dbo].[ENCHERES]
 GO
-IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[ARTICLES_VENDUS]') AND type in (N'U'))
-DROP TABLE [dbo].[ARTICLES_VENDUS]
+IF EXISTS
+    (
+        SELECT
+            *
+        FROM
+            sys.objects
+        WHERE
+            object_id = OBJECT_ID(N'[dbo].[ARTICLES_VENDUS]')
+        AND type IN (
+                        N'U'
+                    )
+    )
+    DROP TABLE [dbo].[ARTICLES_VENDUS]
 GO
-IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[CATEGORIES]') AND type in (N'U'))
-DROP TABLE [dbo].[CATEGORIES]
+IF EXISTS
+    (
+        SELECT
+            *
+        FROM
+            sys.objects
+        WHERE
+            object_id = OBJECT_ID(N'[dbo].[CATEGORIES]')
+        AND type IN (
+                        N'U'
+                    )
+    )
+    DROP TABLE [dbo].[CATEGORIES]
 GO
-IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[UTILISATEURS]') AND type in (N'U'))
-DROP TABLE [dbo].[UTILISATEURS]
+IF EXISTS
+    (
+        SELECT
+            *
+        FROM
+            sys.objects
+        WHERE
+            object_id = OBJECT_ID(N'[dbo].[UTILISATEURS]')
+        AND type IN (
+                        N'U'
+                    )
+    )
+    DROP TABLE [dbo].[UTILISATEURS]
 GO
